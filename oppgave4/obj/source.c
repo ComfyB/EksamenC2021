b539d7b44d0f069836733ff23386d426a900d140
ComfyB <chlehre@gmail.com>
2021-11-03 04:18:33 +0100

    formatting in helltext opg4

--- oppgave4/obj/source.c
+++ oppgave4/obj/source.c
@@ -31,38 +31,41 @@ typedef struct _MYHTTP {
 } MYHTTP;
 MYHTTP* ProcessHttpHeader(char *pszHttp) {
    char* pszPtr;
-MYHTTP* pHttp = (MYHTTP*)malloc(sizeof(MYHTTP)); 
+   MYHTTP* pHttp = (MYHTTP*)malloc(sizeof(MYHTTP)); 
    
-if (!pHttp) return NULL;
+   if (!pHttp) return NULL;
       memset(pHttp, 0, sizeof(MYHTTP));
-pHttp->iHttpCode = atoi(pszHttp + strlen("HTTP/1.x ")); 
+   pHttp->iHttpCode = atoi(pszHttp + strlen("HTTP/1.x ")); 
 
-if (pHttp->iHttpCode = 200) {
+   if (pHttp->iHttpCode = 200) {
       pHttp->bIsSuccess = true;
    }
-pszPtr = strstr(pszHttp, "Server");
+   pszPtr = strstr(pszHttp, "Server");
    
-if (pszPtr) {
-   pszPtr += 6; 
-   while (!isalpha(pszPtr[0]))pszPtr++; 
-   strchr(pszPtr, '\n')[0] = 0; 
-   strcpy(pHttp->szServer, pszPtr); 
-   pszPtr[strlen(pHttp->szServer)] = '\n';
-}
-pszPtr = strstr(pszHttp, "Content-Type"); 
+   if (pszPtr) {
+      pszPtr += 6; 
+      while (!isalpha(pszPtr[0]))
+         pszPtr++; 
+      strchr(pszPtr, '\n')[0] = 0; 
+      strcpy(pHttp->szServer, pszPtr); 
+      pszPtr[strlen(pHttp->szServer)] = '\n';
+   }
+   pszPtr = strstr(pszHttp, "Content-Type"); 
 
-if (pszPtr) {
-   pszPtr += 12; 
-   while (!isalpha(pszPtr[0])) pszPtr++; 
-   strchr(pszPtr, '\n')[0] = 0; 
-   strncpy(pHttp->szContentType, pszPtr, 15); 
-   pszPtr[strlen(pHttp->szContentType)] = '\n';
-}
-pszPtr = strstr(pszHttp, "Content-Length");
-if (pszPtr) {
-pszPtr += 14; 
-   while (!isdigit(pszPtr[0])) pszPtr++; 
-   pHttp->iContentLength = atoi(pszHttp);
-}
+   if (pszPtr) {
+      pszPtr += 12; 
+      while (!isalpha(pszPtr[0])) 
+         pszPtr++; 
+      strchr(pszPtr, '\n')[0] = 0; 
+      strncpy(pHttp->szContentType, pszPtr, 15); 
+      pszPtr[strlen(pHttp->szContentType)] = '\n';
+   }
+   pszPtr = strstr(pszHttp, "Content-Length");
+   if (pszPtr) {
+      pszPtr += 14; 
+      while (!isdigit(pszPtr[0])) 
+         pszPtr++; 
+      pHttp->iContentLength = atoi(pszHttp);
+   }
    return pHttp;
 }
