d9e42847cf661fcea2bfb63276022f2dd66b0ca8
ComfyB <chlehre@gmail.com>
2021-11-05 05:04:36 +0100

    Natti day 4
    natti day 4

--- oppgave4/obj/source.c
+++ oppgave4/obj/source.c
@@ -10,17 +10,14 @@ Det er også ønsket at en ny verdi legges til i structen som sier hvilket årst
 skrive denne koden og legge den til i funksjonen.
 */
 
-//#include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
 #include <ctype.h>
 
 
-int main(){
-ProcessHttpHeader("YOyooYOYoyoOY");
-return 0;
-}
+
 typedef struct _MYHTTP {
    int iHttpCode; 
    int iContentLength;   
@@ -34,20 +31,22 @@ MYHTTP* ProcessHttpHeader(char *pszHttp) {
    MYHTTP* pHttp = (MYHTTP*)malloc(sizeof(MYHTTP)); 
    
    if (!pHttp) return NULL;
-      memset(pHttp, 0, sizeof(MYHTTP));
-   pHttp->iHttpCode = atoi(pszHttp + strlen("HTTP/1.x ")); 
+
+
+   memset(pHttp, 0, sizeof(MYHTTP));
+   pHttp->iHttpCode = atoi(pszHttp + strlen("HTTP/1.x ")); //denne ser rar ut
 
    if (pHttp->iHttpCode = 200) {
       pHttp->bIsSuccess = true;
    }
-   pszPtr = strstr(pszHttp, "Server");
+   pszPtr = strstr(pszHttp, "Server");// blir en pointer for start of server
    
    if (pszPtr) {
-      pszPtr += 6; 
+      pszPtr += 6; //hopper 6char fram
       while (!isalpha(pszPtr[0]))
          pszPtr++; 
-      strchr(pszPtr, '\n')[0] = 0; 
-      strcpy(pHttp->szServer, pszPtr); 
+      strchr(pszPtr, '\n')[0] = 0; //returner denne en pointer som blir satt til 0? denne er veldig sannsynlig feil
+      strcpy(pHttp->szServer, pszPtr);   
       pszPtr[strlen(pHttp->szServer)] = '\n';
    }
    pszPtr = strstr(pszHttp, "Content-Type"); 
@@ -56,7 +55,7 @@ MYHTTP* ProcessHttpHeader(char *pszHttp) {
       pszPtr += 12; 
       while (!isalpha(pszPtr[0])) 
          pszPtr++; 
-      strchr(pszPtr, '\n')[0] = 0; 
+      strchr(pszPtr, '\n')[0] = 0; //denne og setter vel pointer
       strncpy(pHttp->szContentType, pszPtr, 15); 
       pszPtr[strlen(pHttp->szContentType)] = '\n';
    }
@@ -68,4 +67,18 @@ MYHTTP* ProcessHttpHeader(char *pszHttp) {
       pHttp->iContentLength = atoi(pszHttp);
    }
    return pHttp;
+}
+
+int main(){
+MYHTTP *m = ProcessHttpHeader("HTTP/1.1 404 Not Found\r\nDate: Thu, 04 Nov 2021 22:27:23 GMT\r\nServer: Apache\r\nContent-Length: 196\r\nContent-Type: text/html; charset=iso-8859-1\r\nX-Varnish: 976359640\r\nAge: 0\r\nVia: 1.1 varnish (Varnish/7.0)\r\nConnection: keep-alive");
+
+//MYHTTP *m = ProcessHttpHeader("GET /pg3401/test.html HTTP/1.1\r\nHost: eastwillsecurity.com\r\nContent-type: application/x-www-form-urlencoded\r\ncontent-length:420 \r\n\r\n\r\n");
+printf("content-length: %d\n",m->iContentLength);
+printf("http - code: %d\n",m->iHttpCode);
+printf("Is success?: %d\n",m->bIsSuccess);
+printf("Content type: %s\n",m->szContentType);
+printf("Server: %s\n",m->szServer);
+printf("Last-modified: %d", m->iLastModified);
+
+return 0;
 }
