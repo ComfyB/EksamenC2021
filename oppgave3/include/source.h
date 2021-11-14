4ffda646ff8baf2b3623cd519d0254e66475ae88
ComfyB <chlehre@gmail.com>
2021-11-14 02:20:52 +0100

    commenting

--- oppgave3/include/source.h
+++ oppgave3/include/source.h
@@ -7,9 +7,9 @@
 
 typedef struct Node
 {
-    char *VARENAVN[25];
-    int ANTALL;
-    float PRIS;
+    char *ITEMNAME[25];
+    int QUANTITY;
+    float PRICE;
     struct Node *NEXT;
     struct Node *PREV;
 } Node;
@@ -26,11 +26,11 @@ DLL *initDLL(void);
 Node *initNode(void);
 
 void printList(DLL *list);
-void handleInput(DLL *list);
+void inputValidation(DLL *list);
 void cleanExit(DLL *list);
 void deleteWithKey(DLL *list);
 void delNode(DLL *list, Node *_delete);
-void appendNode(DLL *list, char *navn, float pris, int antall);
+void appendNode(DLL *list, char *name, float price, int quantity);
 float sum(DLL *list);
 
 #endif
--- oppgave3/obj/source.c
+++ oppgave3/obj/source.c
@@ -2,44 +2,48 @@
 
 int main(void)
 {
-    //making a class
-    DLL *dll = initDLL();
-    int running = 1;
+
+    DLL *dll = initDLL(); //making a struct to hold head and tail of the list
+    int running = 1;      //int that changes when menu returns 0. Then whileloop stops running
     while (running)
     {
         running = menu(dll);
     }
-    free(dll);
+    free(dll); //free the dll when all is done.
+
     return 0;
 }
-
+//function for memory allocation and make a doubly linked list this function is only called in main in this program
+//but as its a function it makes it easier to scale the program to more than one list if needed later. returns a
+//pointer to a struct that has pointers to head&tail.
 DLL *initDLL(void)
 {
     DLL *newList = malloc(sizeof(*newList));
-    if (newList == NULL)
+    if (newList == NULL) //check if malloc has failed
     {
         printf("error allocation mem for dll");
+        exit(1);
     }
-    memset(newList, 0, sizeof(*newList));
-    newList->head = NULL;
+    memset(newList, 0, sizeof(*newList)); //clear the meory
+    newList->head = NULL;                 //sets head and tail to NULL as we no nodes are created yet.
     newList->tail = NULL;
-    return newList;
+    return newList; //return pointer to the newly created DLL
 }
-
+//function to create a node in the dll. mallocs memory and testes wether it faled or not before memsetting to zero and returning the node.
 Node *initNode(void)
 {
 
     Node *newNode = malloc(sizeof(*newNode));
     if (newNode == NULL)
     {
-        printf("error allocation of mem for node exiting!");
+        printf("error allocation of mem for node!");
         exit(1);
     }
     memset(newNode, 0, sizeof(*newNode));
 
-    return newNode;
+    return newNode; //returns pointer to the newly created node
 }
-
+//menu which takes a DLL as argument.
 int menu(DLL *list)
 {
     char choice;
@@ -48,12 +52,12 @@ int menu(DLL *list)
     printf("\n*********************\n");
     printf("Skriv inn et valg og bekreft med enter:\n");
     printf("1. Legg til vare i slutten av listen:\n");
-    printf("2. Slett forrige/ angre forrige element\n");
+    printf("2. Slett det siste elementet i listen\n");
     printf("3. Fjern alle varer med navn fra listen: \n");
     printf("4. Sum av alle varer: \n");
     printf("5. Kvittering\n");
     printf("6. Exit program\n");
-    while (1)
+    while (1) //input whilw 1, will break out of the loop if the inpud is valid.
     {
         if (scanf("%c", &choice))
             ;
@@ -62,34 +66,37 @@ int menu(DLL *list)
         if (choice != '\n')
             printf("%s is not a valid menu option!\n", &choice);
     }
-    switch (choice)
-    {
+    switch (choice) //we already know that the choice is valid. so now the menu calls the different functions needed in the dll
+    {               //all the cases return 1, so that the while in main takes as is whatever the menu function returns still runs.
     case '1':
-        handleInput(list);
+        inputValidation(list); //new node input
         return 1;
     case '2':
-        delNode(list, list->tail);
+        delNode(list, list->tail); //delets the tail with the delete function.
         return 1;
     case '3':
-        deleteWithKey(list);
+        deleteWithKey(list); //starts the funcion which delete with key
         return 1;
     case '4':
-        printf("\nTotal: %.2f\n", sum(list));
+        printf("\nTotal: %.2f\n", sum(list)); //returns the sum and prints it in terminal.
         return 1;
     case '5':
-        printList(list);
+        printList(list); //print the entire list
         return 1;
     case '6':
-        cleanExit(list);
+        cleanExit(list); //runs the cleanup process to free memory and void leaks before setting "running" in main to 0, which exits the loop in main
         return 0;
-    default:
-        printf("something went wrong");
+    default: //should never happen but in case something went wrong we delete the list and exit the program
+        printf("\nsomething went wrong cleaning and exiting\n");
         cleanExit(list);
         return 0;
     }
-    cleanExit(list);
+    cleanExit(list); //should never happen but in case something went wrong we delete the list and exit the program
+    printf("\nsomething went wrong cleaning and exiting\n");
     return 0;
 }
+//Using input from the user this function compares the node->ITEMNAME if its the same as an input. When matched it sends the node pointer to the
+//delete fnction
 void deleteWithKey(DLL *list)
 {
     if (list->head == NULL)
@@ -102,59 +109,65 @@ void deleteWithKey(DLL *list)
     char key[25];
     if (scanf("%s", key))
         ;
-    i = list->head;
-    while (i->NEXT != NULL)
+    i = list->head;         //sets i to head
+    while (i->NEXT != NULL) //then count through the nodes
     {
-        tmp = i->NEXT;
-        if (strcmp(*i->VARENAVN, key) == 0)
+        tmp = i->NEXT;                      //save temporary as you lose the address when deleting i->NEXT
+        if (strcmp(*i->ITEMNAME, key) == 0) //using strcmp to see if the key and node->VARENAVM matches.
             delNode(list, i);
         i = tmp;
     }
 }
+//function to iterate through the list and print as it goes through. will break the look of the recipt if the name of the item is longer than
+//name plus tab. Creates a temporary pointer so we can update it and pull data from all nodes. Then sends the list to the sum function which
+//returns the total
 void printList(DLL *list)
 {
-    Node *i ;
+    Node *i;
     printf("*********************************\n");
-    printf("navn\tpris\tantall\tvaretotal\n");
+    printf("navn\tpris\tQUANTITY\tvaretotal\n");
     printf("_________________________________\n\n");
 
     for (i = list->head; i != NULL; i = i->NEXT)
     {
-
-        printf("%s\t", *i->VARENAVN);
-        printf("%.2f\t", i->PRIS);
-        printf("%d\t", i->ANTALL);
-        printf("%.2f\n", (i->PRIS * i->ANTALL));
+        //printing the recipt
+        printf("%s\t", *i->ITEMNAME);
+        printf("%.2f\t", i->PRICE);
+        printf("%d\t", i->QUANTITY);
+        printf("%.2f\n", (i->PRICE * i->QUANTITY));
     }
     printf("_________________________________\n\n");
 
-    printf("\033[0;31m\t\tTotal \t%.2f;-\n\033[0;0m", sum(list));
+    printf("\033[0;31m\t\tTotal \t%.2f;-\n\033[0;0m", sum(list)); //playing with linux terminal settings where \033 is the escape character [0;31m gives the color
     printf("*********************************\n");
-
-} //print all nodes into terminal
-
-void handleInput(DLL *list)
+}
+//function for inputchecking passing arguments to a function for appending the node. Reasoning behind making it a seperate function is that
+//if you want to extend the program later with other ways of inserting the values of the node that is not just to the end of the list, we can use
+//the same inputvalidator
+void inputValidation(DLL *list)
 {
 
-    char *navn = malloc(sizeof(char) * 25);
-    if (navn == NULL){
+    char *name = malloc(sizeof(char) * 25);
+    if (name == NULL)
+    {
         printf("error while allocation of navn variable");
-        exit(1);}
-
-    float pris;
-    int antall;
+        exit(1);
+    }
+    //local variables for the inputted values
+    float price;
+    int quantity;
     int i = 0;
 
     printf("\n*********************\n");
 
-    printf("Oppgi et varenavn og trykk enter:  ");
-    if (scanf("%25s", navn))
+    printf("Oppgi et navn og trykk enter:  ");
+    if (scanf("%25s", name))
         ;
     printf("\nOppgi en pris og trykk enter:  ");
-    while (1)
+    while (1) //while loop that gets the break; keyword escaping the loop if scanf gets a float as input, if input is valid scanf returns 1;
     {
 
-        if (scanf("%f", &pris)) //,printf("\n")
+        if (scanf("%f", &price)) //,printf("\n")
             break;
         //empty stdin so we get a proper input.
         while (fgetc(stdin) == 1)
@@ -162,19 +175,18 @@ void handleInput(DLL *list)
             i = 0;
         }
         i++;
-        if (i % 3 == 0 || i == 0)
-            printf(". \n");
+        if (i % 3 == 0 || i == 0) //print a dot every third character cleared from stdin.
+            printf(". clearing cache\n");
         if (i == 1)
-            printf("please insert a valid number:\n");
+            printf("please insert a valid number:\n"); // written once for proper input.
     }
 
-    printf("\nOppgi antall varer og trykk enter:  ");
-    while (1)
+    printf("\nOppgi mengde varer og trykk enter:  ");
+    while (1) //same while loop as over just with an input of int and not float.
     {
 
-        if (scanf("%d", &antall)) //,printf("\n")
+        if (scanf("%d", &quantity)) //,printf("\n")
             break;
-        //empty stdin so we get a proper input.
         while (fgetc(stdin) == 1)
         {
             i = 0;
@@ -185,17 +197,17 @@ void handleInput(DLL *list)
         if (i == 1)
             printf("please insert a valid number:\n");
     }
-    appendNode(list, navn, pris, antall);
+    appendNode(list, name, price, quantity); //pass the values to the function that appends a node to the list
 }
 
-void appendNode(DLL *list, char *navn, float pris, int antall)
+void appendNode(DLL *list, char *navn, float price, int QUANTITY)
 {
 
     Node *newNode = initNode();
 
-    *newNode->VARENAVN = navn;
-    newNode->PRIS = pris;
-    newNode->ANTALL = antall;
+    *newNode->ITEMNAME = navn;
+    newNode->PRICE = price;
+    newNode->QUANTITY = QUANTITY;
 
     if (list->head == NULL)
     {
@@ -222,7 +234,7 @@ void delNode(DLL *list, Node *_delete)
     {
         list->tail = NULL;
         list->head = NULL;
-        free(*_delete->VARENAVN);
+        free(*_delete->ITEMNAME);
         free(_delete);
         return;
     }
@@ -241,7 +253,7 @@ void delNode(DLL *list, Node *_delete)
     if (_delete->NEXT == NULL)
         list->tail = _delete->PREV;
 
-    free(*_delete->VARENAVN);
+    free(*_delete->ITEMNAME);
     free(_delete);
 }
 float sum(DLL *list)
@@ -250,7 +262,7 @@ float sum(DLL *list)
     Node *i;
 
     for (i = list->head; i != NULL; i = i->NEXT)
-        sum += i->PRIS * i->ANTALL;
+        sum += i->PRICE * i->QUANTITY;
 
     return sum;
 } //sum pris på alle varer i listen
